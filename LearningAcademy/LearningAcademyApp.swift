a78b0a11f22610fbfe0ade7188159313627e6f53
manishparihar948 <manish.parihar948@gmail.com>
2023-10-24 10:32:40 +0200

    Project Setup

---
+++ LearningAcademy/LearningAcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAcademyApp.swift
+//  LearningAcademy
+//
+//  Created by Manish Parihar on 24.10.23.
+//
+
+import SwiftUI
+
+@main
+struct LearningAcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
